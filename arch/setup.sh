7a0c6e6f71d27296131a53c8187ed6e857097e63
Armando Rios <armandorios.dev@gmail.com>
2025-05-16 15:25:08 -0700

    feat: create function for install arch packages

--- arch/setup.sh
+++ arch/setup.sh
@@ -4,3 +4,8 @@ update_system() {
   echo "Updating system"
   sudo pacman -Syu --noconfirm
 }
+
+install_packages() {
+  echo "Installing packages"
+  sudo pacman -S --needed --noconfirm git base-devel nvm neovim ripgrep fzf gcc waybar hyprpaper hyprsunset swaync zsh ghostty discord lazygit steam
+}
